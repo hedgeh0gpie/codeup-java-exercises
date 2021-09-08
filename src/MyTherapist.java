8d18fc64a22b6467c355e53d862a97d7a6de2cdc
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2021-09-08 09:31:50 -0500

    Add enhanced switch statement and toLowerCase() method

--- src/MyTherapist.java
+++ src/MyTherapist.java
@@ -3,6 +3,7 @@
 //      Based on your response, it should give you some sort of advice.
 
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class MyTherapist {
@@ -11,13 +12,30 @@ public class MyTherapist {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("How are you doing today?");
-        String userResponse = scanner.nextLine();
+        String userResponse = scanner.nextLine().toLowerCase();
 
         switch (userResponse) {
-            case "Great" -> System.out.println("That makes me very happy to hear :)");
-            case "So-so" -> System.out.println("Don't be too hard on yourself! Things are improving even if you don't perceive it!");
-            case "Sad" -> System.out.println("It's okay! Just let it out. Sometimes we all need a good shoulder to cry on. Unfortunately, because I am a chat bot, I cannot provide one.");
-            default -> System.out.println("Please insert one quarter or the equivalent in Bitcoin to continue. Therapy ain't cheap!");
+            case "great":
+                System.out.println("That makes me very happy to hear :)" );
+                break;
+            case "so-so":
+                System.out.println("Don't be too hard on yourself! Things are improving even if you don't perceive it!");
+                break;
+            case "sad":
+                System.out.println("It's okay! Just let it out. Sometimes we all need a good shoulder to cry on. Unfortunately, because I am a chat bot, I cannot provide one.");
+                break;
+            default:
+                System.out.println("Please insert one quarter or the equivalent in Bitcoin to continue. Therapy ain't cheap!");
+                break;
+
+
+
+
+//            switch (userResponse) {
+//                case "Great" -> System.out.println("That makes me very happy to hear :)");
+//                case "So-so" -> System.out.println("Don't be too hard on yourself! Things are improving even if you don't perceive it!");
+//                case "Sad" -> System.out.println("It's okay! Just let it out. Sometimes we all need a good shoulder to cry on. Unfortunately, because I am a chat bot, I cannot provide one.");
+//                default -> System.out.println("Please insert one quarter or the equivalent in Bitcoin to continue. Therapy ain't cheap!");
         }
 
     }
