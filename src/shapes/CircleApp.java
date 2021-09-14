cf62f1fce176b42fdb825c0b24a5b58b017ae3e0
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2021-09-14 15:30:16 -0500

    Add completed Circle class exercise

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,15 @@
+package shapes;
+
+import util.Input;
+
+public class CircleApp {
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.println("Enter the radius of a circle");
+        double radius = input.getDouble();
+        Circle circle  = new Circle(radius);
+        System.out.println(circle.getArea());
+        System.out.println(circle.getCircumference());
+    }
+}
