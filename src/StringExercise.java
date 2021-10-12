5620caa2d4000f679502d749bb5bd53e7583c3bc
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2021-10-11 23:51:40 -0500

    Complete String Basics exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,16 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String str1 = "We don't need no education\nWe don't need no thought control\n";
+        String str2 = "Check \"this\" out!, \"s inside of \"s!\n";
+        String str3 = "In windows, the main drive is usually C:\\\n";
+        String str4 = """
+                I can do backslashes \\, double backslashes \\\\,
+                and the amazing triple backslash \\\\\\!
+                """;
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str4);
+    }
+}
