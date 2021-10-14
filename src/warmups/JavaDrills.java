7e4d4bb9bbed3b852bbbf31a426239d83a5fd2c0
Sebastian McKelvey <sebastian.mckelvey@gmail.com>
2021-10-14 09:26:25 -0500

    Complete warmup for 10/14

---
+++ src/warmups/JavaDrills.java
@@ -0,0 +1,31 @@
+// In `codeup-java-exercises`, create a class `JavaDrills`, with the following:
+// Create a public static method, `flipOuterCase`, that takes in a string and returns the string with the case reversed for the first and last letters. Assume the input will be three or more characters long.
+//        Example inputs and expected outputs...
+//        System.out.println(flipOuterCase(“cat”)); // CaT
+//        System.out.println(flipOuterCase(“CaT”)); // cat
+//        System.out.println(flipOuterCase(“caT”)); // Cat
+//        System.out.println(flipOuterCase(“cAt”)); // CAT
+
+package warmups;
+
+public class JavaDrills {
+
+// Source: https://stackoverflow.com/questions/1729778/how-can-i-invert-the-case-of-a-string-in-java
+    public static String flipOuterCase(String str) {
+        char[] chars = str.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isUpperCase(chars[i])) {
+                chars[i] = Character.toLowerCase(chars[i]);
+            } else if (Character.isLowerCase(chars[i])) {
+                chars[i] = Character.toUpperCase(chars[i]);
+            }
+        }
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(JavaDrills.flipOuterCase("cAt"));
+    }
+
+}
